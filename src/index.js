24ccee3b09665930563f31da485447a069651c29
azarovalex <alex@azarov.by>
2018-02-24 18:16:21 +0300

    Solution.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var tokens = equation.split(" ");
+  var a = Number(tokens[0]);
+  var b = Number(tokens[3] + tokens[4]);
+  var c = Number(tokens[7] + tokens[8]);
+  
+  var discriminant = b * b - 4 * a * c;
+  var x1 = (- b + Math.sqrt(discriminant)) / (2 * a);
+  var x2 = (- b - Math.sqrt(discriminant)) / (2 * a);
+  
+  return [Math.round(x1), Math.round(x2)].sort( (a, b) => (a > b) );
 }
